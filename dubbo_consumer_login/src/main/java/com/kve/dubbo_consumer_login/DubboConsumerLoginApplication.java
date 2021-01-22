0db20a2445d733fd344adc3aea61d5eb8b6bbfac
swagere <1250539180@qq.com>
2021-01-22 17:06:13 +0800

    add new files

--- dubbo_consumer_login/src/main/java/com/kve/dubbo_consumer_login/DubboConsumerLoginApplication.java
+++ dubbo_consumer_login/src/main/java/com/kve/dubbo_consumer_login/DubboConsumerLoginApplication.java
@@ -3,6 +3,7 @@ package com.kve.dubbo_consumer_login;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@
 @SpringBootApplication
 public class DubboConsumerLoginApplication {
 
