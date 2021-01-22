09f867aea9ab1d70a807533dfd04a8dc5ad00931
swagere <1250539180@qq.com>
2021-01-22 16:07:29 +0800

    init

---
+++ dubbo_consumer_login/src/test/java/com/kve/dubbo_consumer_login/DubboConsumerLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kve.dubbo_consumer_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboConsumerLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
